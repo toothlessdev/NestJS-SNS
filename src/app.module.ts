1b51c43c0c541be0e0c662ce9bb3e9dc6bf673c8
toothlessdev <toothless042@gmail.com>
2024-01-01 16:23:39 +0900

    init: Re-Initialize NestJS Project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { PostsModule } from "./posts/posts.module";
-
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { PostsModel } from "./posts/entities/posts.entities";
 
 @Module({
-  imports: [
-    PostsModule,
-    TypeOrmModule.forRoot({
-      type: "postgres", // 데이터베이스 타입
-      host: "127.0.0.1",
-      port: 5432,
-      username: "postgres",
-      password: "postgres",
-      database: "postgres",
-      entities: [PostsModel], // 생성할 db 모델이 들어감
-      synchronize: true,
-      // nestjs typeorm 코드와 db 동기화 (개발환경에서는 true, production 시 false)
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
